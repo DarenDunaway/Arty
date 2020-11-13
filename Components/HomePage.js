8541f2607deb3265742bbd27e32ec799f14591b6
Daren Dunaway <darendunaway@Darens-MacBook-Pro.local>
2020-11-13 14:22:19 -0800

    Added a modal view for the comments section on the example card.

--- Components/HomePage.js
+++ Components/HomePage.js
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import {useState} from 'react';
-import { View, Text, Image, StyleSheet, SafeAreaView, StatusBar } from 'react-native'
+import { View, Text, Image, StyleSheet, SafeAreaView, StatusBar , Alert, Modal, TouchableHighlight} from 'react-native'
 import { Card, ListItem, Button } from 'react-native-elements';
 import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Modal from 'react-native-modal';
 import PostPage from './PostPage'
 
 const MainStack = createStackNavigator();
 
 function HomeScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#6a51ae" />
@@ -25,9 +25,33 @@ function HomeScreen() {
             Caption
           </Text>
           <Card.Divider/>
-          <Button
-             buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-             title='Comments' />
+            <Modal
+             animationType="none"
+             transparent={true}
+             visible={modalVisible}
+             >
+              <View style={styles.centeredView}>
+                 <View style={styles.modalView}>
+                   <Text style={styles.modalText}>Display comments and add ability to create comments</Text>
+                   <TouchableHighlight
+                     style={{ ...styles.openButton, backgroundColor: "black" }}
+                     onPress={() => {
+                     setModalVisible(!modalVisible);
+                     }}
+                   >
+                   <Text style={styles.textStyle}>Close Comments</Text>
+                   </TouchableHighlight>
+                 </View>
+               </View>
+          </Modal>
+         <TouchableHighlight
+           style={styles.openButton}
+           onPress={() => {
+             setModalVisible(true);
+            }}
+          >
+          <Text style={styles.textStyle}>Comments</Text>
+      </TouchableHighlight>
         </Card>
       </View>
     </SafeAreaView>
@@ -75,4 +99,41 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 1
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "grey",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 10
+  },
+  openButton: {
+    backgroundColor: "black",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 10
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    color: "black"
+  }
 });
